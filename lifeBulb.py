e980c037cc0a76d5096377dbf114f4c2a1eb5e8c
sampaulson <sjpaulson4@gmail.com>
2018-11-15 14:54:11 -0500

    Code Review 2.1
    - Now uses regex to parse an integer from the live data from the API. While the current API is just a placeholder, getting regex to work properly was a good (and satisfying) step forward.
    - Uses this integer to set the incandescence of the 3D lightbulb
    - Program renders an image of the bulb after the incandescence has been set
    - While each section of the code runs properly when run individually, the program does not currently run as a whole, because Maya won't recognize the "requests" library yet

---
+++ lifeBulb.py
@@ -0,0 +1,28 @@
+import requests
+import re
+import maya.cmds as cmds
+
+#This function takes a float from a live data set and scales it to the range
+#0 to 1, which is the range of the intensity attibrute on the light object
+#in Maya. It then sets the intensity attribute to the value of the scaled
+#argument
+
+#here: imports data from an api and uses regex to parse out a single value
+headers = {
+  'Accept': 'application/json'
+}
+
+whole_data = requests.get('https://api.carbonintensity.org.uk/intensity', params={}, headers = headers)
+data_text = str(whole_data.json())
+
+data_text_1stpass = str(re.findall(r'\'actual\': \w\w\w\b', data_text))
+data_text_2ndpass = re.findall(r'\d\d\d', data_text_1stpass)
+population = int(data_text_2ndpass[0])
+
+#here: takes the value parsed through regex and uses it to set the
+#      relative incandescence of the LifeBulb
+max = 500 #this value is arbitrary
+incand = population/max
+
+cmds.setAttr("bulbShade.incandescence",incand,incand,incand, type="double3")
+cmds.render("renderCam")

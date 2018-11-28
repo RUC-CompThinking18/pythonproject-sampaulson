dd53635c92445baba82bc829d50e5de72f7d580e
sampaulson <sjpaulson4@gmail.com>
2018-11-27 20:59:06 -0500

    Final Submission
    Removed live data from sample API and instead extrapolated data from existing findings. Added a lot of comments to explain what each section of the code is doing. Added a READ_ME file that includes my concept and project narrative.

---
+++ READ_ME.txt
@@ -0,0 +1,38 @@
+My project takes the rate at which ALL species on Earth are going extinct, and
+dims a CG light bulb at the same rate. When all species have
+gone extinct, the "Lifebulb" won't be lit any longer.
+
+The Lifebulb satirizes the first world’s exaltation of energy and the
+commodities that come with it (light, appliances, etc) by drawing a connection
+between energy and the environment. The destruction of habitats and the
+consequential endangerment of species are common knowledge, but they are not
+universally acknowledged. So, in an attempt to grab the attention of people
+who would not otherwise care, the subject of this piece is something we
+in the first world are all very used to acknowledging: a light bulb.
+
+The fulfillment of many of our first world whims is reliant on our access to
+light, and consequently, it is often reflexive to replace an old bulb with a
+new one. We are so quick to take action, because we are inconvenienced
+personally and immediately. But the same cannot be done with an extinct
+species — and we don’t seem to care. By equating the decline of a population
+to the dimming of a light bulb, I am criticizing the selfishness of the first
+world and arguing that we should be giving as much attention to the
+preservation of species as we are giving to the fulfillment of our whims.
+
+* * *
+
+Coding the Lifebulb was not particularly difficult: from the start I had a
+pretty good understanding of how I would work with the data. What was troubling,
+though, was figuring out how to get Maya to import the module that I was
+fixed on using, which was 'requests.' However, two realizations convinced me
+to take a different approach: first, there was no way that I was going to find
+an API that would feed the data to me live, and second, it would be way easier
+to extrapolate the data from existing findings. So that is exactly what I did,
+and Maya was perfectly cooperative, since it already had the modules that I
+would need to do this ('datetime' and 're').
+
+Having completed this project, I am very pleased that I've finally coded
+something for Maya! This is something that I've wanted to try for about a year,
+but I couldn't do it because I didn't have any experience coding in Python
+(or MEL, Maya's proprietary language). Now I am excited to tackle new code-
+oriented projects in Maya :D
--- lifeBulb.py
+++ lifeBulb.py
@@ -1,28 +1,47 @@
-import requests
 import re
-import maya.cmds as cmds
+from datetime import date
 
-#This program takes a float from a live data set and scales it to the range
-#0 to 1, which is the range of the incandescence attibrute on the "lightbulb" object
-#in Maya. It then sets the incandescence attribute to the value of the scaled
-#float
+#As reported by BBC on August 23, 2011 (https://www.bbc.com/news/science-environment-14616161),
+#scientists estimate that the natural world contains about 8.7 mil species
+date0 = date(2011, 8, 23)
+num_of_species_at_date0 = 8700000.0
 
-#here: imports data from an api and uses regex to parse out a single value
-headers = {
-  'Accept': 'application/json'
-}
+#This function calculates the number of days that have passed
+#since it was determined that there are ~8.7 extant species
+def days_since_date0():
+    date_current = date.today()
+    time_since_date0 = str(date_current - date0)
+    #Since this datetime operation outputs a datetime object, we'll convert
+    #it to a string and parse out the number of days that have passed
+    #with regex
+    days_since_date0_1stpass = str(re.findall(r'\w* days\b', time_since_date0))
+    days_since_date0_2ndpass = re.findall(r'\d', days_since_date0_1stpass)
 
-whole_data = requests.get('https://api.carbonintensity.org.uk/intensity', params={}, headers = headers)
-data_text = str(whole_data.json())
+    days = ""
+    for element in days_since_date0_2ndpass:
+        days += element
 
-data_text_1stpass = str(re.findall(r'\'actual\': \w\w\w\b', data_text))
-data_text_2ndpass = re.findall(r'\d\d\d', data_text_1stpass)
-population = int(data_text_2ndpass[0])
+    return float(days)
 
-#here: takes the value parsed through regex and uses it to set the
-#      relative incandescence of the LifeBulb
-max = 500
-incand = population/max
+#This function calculates how many species we have lost since August 23, 2011
+#and divides that number by 8.7 mil to determine the percentage remaining.
+#Because, like percentages, the Lifebulb's incandescence must be a value
+#between 0 and 1, the percentage value is used to set the incandescence.
+def find_incandescence():
+    #The World Wildlife Fund determined that we lose about
+    #150 species every day (http://wwf.panda.org/our_work/biodiversity/biodiversity/)
+    species_lost_per_day = 150
+    species_lost_since_date0 = species_lost_per_day * days_since_date0()
+    num_of_species_remaining = num_of_species_at_date0 - species_lost_since_date0
+    if num_of_species_remaining <= 0:
+        return 0
 
+    incandescence = num_of_species_remaining/num_of_species_at_date0
+    return incandescence
+
+incand = find_incandescence()
+print incand
+#Finally, we'll set the new incandescence of the Lifebulb.
 cmds.setAttr("bulbShade.incandescence",incand,incand,incand, type="double3")
+#And lastly, we'll render an image of the bulb with its new incandescence.
 cmds.render("renderCam")
